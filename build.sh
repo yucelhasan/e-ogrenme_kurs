c359e4841d996acc8858c60a73bf3f2ccd40c316
Hasan Yücel <hasanyc341@gmail.com>
2026-05-03 12:08:02 +0300

    .

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --no-input
+python manage.py createsuperuser --noinput --username $DJANGOSUPERUSER_USERNAME --email $DJANGOSUPERUSER_EMAIL
